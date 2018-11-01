5ed2a4e577fe85f12d3accf24b803eae50a3a9ab
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-01 01:08:14 -0500

    UI and logic for adding sales to each sales rep - DONE

--- client/components/RepList.js
+++ client/components/RepList.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class RepList extends Component {
+	onAddSales (id) {
+		console.log(this.props);
+		this.props.mutate({
+			variables: { id },
+		});
+	}
+
 	renderReps (props) {
-		return this.props.reps.map(({ id, repName }) => {
+		return this.props.reps.map(({ id, repName, sales }) => {
 			return (
 				<li key={id} className="collection-item">
 					{repName}
+					<div className="right-align">
+						<i onClick={() => this.onAddSales(id)} className="material-icons">
+							add_box
+						</i>
+						<span className="new badge" data-badge-caption="sales">
+							{sales}
+						</span>
+					</div>
 				</li>
 			);
 		});
 	}
 
 	render () {
-		return <ul className="collection">{this.renderReps()}</ul>;
+		return (
+			<div>
+				<ul className="collection">{this.renderReps()}</ul>
+			</div>
+		);
 	}
 }
 
-export default RepList;
+const mutation = gql`
+	mutation AddSalesToRep($id: ID) {
+		addSalesToRep(id: $id) {
+			id
+			sales
+		}
+	}
+`;
+
+export default graphql(mutation)(RepList);
--- client/queries/fetchReps.js
+++ client/queries/fetchReps.js
@@ -8,6 +8,7 @@ export default gql`
 			reps {
 				id
 				repName
+				sales
 			}
 		}
 	}
